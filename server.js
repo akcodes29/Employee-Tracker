4af8cab49c189ab25974550bef022d64c8e3e4a9
Andrea <keriazes.andrea@gmail.com>
2023-09-23 16:05:40 -0400

    added .then statement for update employee role

--- server.js
+++ server.js
@@ -85,7 +85,8 @@ const createDepartment = () => {
                 `INSERT INTO department(name) VALUES('${answers.departmentName}')`,
                 function (err, results) {
                     console.log(err);
-                    console.log(results); // results contains rows returned by server
+                    console.table(results); // results contains rows returned by server
+                    askUser()
 
                 }
             )
@@ -125,7 +126,7 @@ const addRole = () => {
             {
                 type: "list",
                 name: "departmentId",
-                message: "Enter the department",
+                message: "Choose the department",
                 choices: departmentList
             }
         ])
@@ -174,7 +175,7 @@ const addEmployee = () => {
         },
         {
             type: "list",
-            name: "manaager_id",
+            name: "manager_id",
             message: "Who is the employee's manager",
             choices: ['None', 'Drake', 'Taylor Swift', 'Kenny Chesney', 'Morgan Wallen', 'Jesus']
         }
@@ -185,7 +186,7 @@ const addEmployee = () => {
                 `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('${answers.first_name}', '${answers.last_name}', '${answers.role_id}', '${answers.manager_id}')`,
                 function (err, results) {
                     console.log(err);
-                    console.log(results); // results contains rows returned by server
+                    console.table(results); // results contains rows returned by server
 
                 }
             )
@@ -202,4 +203,47 @@ const showAllEmployees = () => {
             askUser()
         }
     );
+}
+
+const updateEmployeeRole = () => {
+
+    connection.query('select * from employee', function (err, res) {
+        const employeeList = res.map(({id, first_name, last_name}) => ({
+            name: first_name + ' ' + last_name,
+            value: id, 
+        }))
+
+     connection.query('select * from role', function (err, res) {
+            const roleList = res.map(({ id, title }) => ({
+                name: title ,
+                value: id
+            }))
+  
+     inquirer.prompt([
+        {
+            type: "list",
+            name: "updateER",
+            message: "Which employee's role do you want to update?",
+            choices: employeeList
+        },
+        {
+            type: "list",
+            name: "updateRoleTitle",
+            message: "Which role do you want to assign the selected employee?",
+            choices: roleList
+        }
+    ])
+    .then((answers) => {
+        console.log(answers)
+        connection.query(
+            `UPDATE employee SET role_id = ('${answers.updateRoleTitle}') WHERE employee.role_id`,
+            function (err, results) {
+                console.log(err);
+                console.log('new role has been added!'); // results contains rows returned by server
+                askUser()
+            }
+        )
+    })
+   })
+ })
 }
